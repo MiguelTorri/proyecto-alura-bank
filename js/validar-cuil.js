d86c7d37f457a2f0ae73d83dd8392e9736274763
Miguel Torrico <mickimaick18@gmail.com>
2024-05-20 09:39:46 -0400

    validando los primeros 2 digitos del cuil

--- js/validar-cuil.js
+++ js/validar-cuil.js
@@ -2,11 +2,14 @@ export default function esUnCuil(campo){
     const cuil = campo.value.replace(/[-\/]/g,"");
     tieneNumerosRepetidos(cuil)
 
-    console.log(cuil);
-    console.log(tieneNumerosRepetidos(cuil));
+    //console.log(cuil);
+    //console.log(tieneNumerosRepetidos(cuil));
+
+    validarPrimerosDigitos(cuil)
+    console.log(validarPrimerosDigitos(cuil));
 }
 
-
+//evitar numeros repetidos
 function tieneNumerosRepetidos(cuil){
     const numeroRepetidos = [
         "00000000000",
@@ -22,4 +25,10 @@ function tieneNumerosRepetidos(cuil){
     ];
     
     return numeroRepetidos.includes(cuil);
+}
+
+function validarPrimerosDigitos(cuil){  //extrae los primeros 2 caracteres del campo cuil
+    let primerosDigitos = cuil.substr(0,2);
+    let digitosValidos = ["20", "23", "24", "27", "30", "33", "34"];
+    return digitosValidos.includes(primerosDigitos); //retornara un true si es que contiene los digitos y false si no los contine
 }
