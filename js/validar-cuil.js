cba04eb464ec845263fca2c05af790e48ee97556
Miguel Torrico <mickimaick18@gmail.com>
2024-05-21 10:31:45 -0400

    testeo de validar codigo en cuil

--- js/validar-cuil.js
+++ js/validar-cuil.js
@@ -5,8 +5,10 @@ export default function esUnCuil(campo){
     //console.log(cuil);
     //console.log(tieneNumerosRepetidos(cuil));
 
-    validarPrimerosDigitos(cuil)
-    console.log(validarPrimerosDigitos(cuil));
+    //validarPrimerosDigitos(cuil)
+    //console.log(validarPrimerosDigitos(cuil));
+    validarDigitoVerificador(cuil)
+    console.log(validarDigitoVerificador(cuil));
 }
 
 //evitar numeros repetidos
@@ -31,4 +33,25 @@ function validarPrimerosDigitos(cuil){  //extrae los primeros 2 caracteres del c
     let primerosDigitos = cuil.substr(0,2);
     let digitosValidos = ["20", "23", "24", "27", "30", "33", "34"];
     return digitosValidos.includes(primerosDigitos); //retornara un true si es que contiene los digitos y false si no los contine
+}
+
+function validarDigitoVerificador(cuil){
+    let acumulado = 0;
+    const factores = [5,4,3,2,7,6,5,4,3,2];
+
+    for(let i=0; i<10; i++){
+        acumulado += parseInt(cuil[i],10) * factores[i];
+    }
+
+    let ValidadorTeorico = 11 - (acumulado % 11)
+
+    if(ValidadorTeorico==11){
+        ValidadorTeorico = 0
+    }else if(ValidadorTeorico == 10){
+        ValidadorTeorico = 9
+    }
+
+    const digitoVerificador = parseInt(cuil[10],10)
+
+    return digitoVerificador === ValidadorTeorico;
 }
